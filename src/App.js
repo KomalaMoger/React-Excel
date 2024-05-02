b24f8c90961766eaa07c5babb06cb0b2e0e41e1b
KomalaMoger <komalamoger07@gmail.com>
2024-05-02 17:19:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {CSVLink} from 'react-csv'
+
+const headers=[
+  {label:'userId',key:'id'},
+  {label:'userName',key:'name'},
+  {label:'userLocation',key:'city'}
+]
+
+const users=[
+  {id:1,name:'Nisha',city:'Delhi'},
+  {id:2,name:'mamata',city:'mysore'},
+  {id:3,name:'raju',city:'bangalore'},
+  {id:4,name:'sneha',city:'Noida'}
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React CSV Example</h1>
+      <CSVLink data={users} headers={headers}>
+      <button>Export Users Data</button>
+      </CSVLink>
+     
     </div>
   );
 }
